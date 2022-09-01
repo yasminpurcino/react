f135100257d1a4c07d7b8742c5df0c56a4c34188
yasminpurcino <108705820+yasminpurcino@users.noreply.github.com>
2022-09-01 13:40:33 +1200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:17-alpine
+WORKDIR /
+COPY . .
+EXPOSE 3000
+RUN npm install
+CMD ["npm","start"]
---
+++ public/example.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+  // boolean my_bool=false
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Toggle />);
